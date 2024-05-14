576442fadc0b6d2fdba11d42d3008daa5696ac3a
Ali Rezaei <im1.bitcode@gmail.com>
2024-05-14 22:54:16 +0330

    start

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="p-10">
+      <Button>salam</Button>
+    </div>
+  );
+}
+
+export default App;
