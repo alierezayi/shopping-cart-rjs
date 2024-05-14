27a3f35429b48a271d828e1325abae5a674242a5
Ali Rezaei <im1.bitcode@gmail.com>
2024-05-14 23:13:32 +0330

    rouating

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Button } from "./components/ui/button";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import NotFoundPage from "./pages/404";
 
 function App() {
   return (
-    <div className="p-10">
-      <Button>salam</Button>
-    </div>
+    <Routes>
+      <Route index element={<Navigate to="products" replace />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
